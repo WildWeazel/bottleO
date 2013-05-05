81da541930b8abdb5ac59a48c623baf610f0a049
Travis Christian <travis.w.christian@gmail.com>
2013-05-05 15:19:17 -0700

    stripping down plugin to only bottle XP

--- src/com/untamedears/bottleO/bottleO.java
+++ src/com/untamedears/bottleO/bottleO.java
@@ -3,37 +3,24 @@ package com.untamedears.bottleO;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class bottleO extends JavaPlugin {
-	
+
     public static Logger log;
     public EventListener listener;
-    public static String pluginName = "bottleO";
-    public static String version = "0.7.6";
-    
+    public static String pluginName = "bottleO lite";
+    public static String version = "0.1.0";
+
 	public void onEnable() {
 		log = this.getLogger();
-		
+
 		listener = new EventListener(this);
 		Bukkit.getPluginManager().registerEvents(listener, this);
-		
-		//add recipes
-		ShapelessRecipe expToEmeraldRecipe = new ShapelessRecipe(new ItemStack(Material.EMERALD,1));
-		for (int i = 0; i < 9; i++) {
-			expToEmeraldRecipe.addIngredient(Material.EXP_BOTTLE);
-		}
-		Bukkit.addRecipe(expToEmeraldRecipe);
-		ShapelessRecipe emeraldToExpRecipe = new ShapelessRecipe(new ItemStack(Material.EXP_BOTTLE,9));
-		emeraldToExpRecipe.addIngredient(Material.EMERALD);
-		Bukkit.addRecipe(emeraldToExpRecipe);
-		
+
 		log.info(pluginName+" v"+version+" enabled!");
 	}
-	
+
 	public void onDisable() {
 		log.info(pluginName+" v"+version+" disabled!");
 	}
